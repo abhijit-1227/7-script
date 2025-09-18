41641a180f5e66a0b55995a2046aee13d0b5c76d
abhijit-1227 <digraskarabhijit123@gmail.com>
2025-09-18 11:38:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// console.log("external" );
+// alert("variable in js");
+// cosole.log("abhi dada");
+//  var age=21;
+// console.log(age);
+// let x =10;
+
+// console.log("x isvalue "+x)
+
+// var value;
+// console.log(value)
+// value="java script";
+// console.log(value)
+
+
+//data types
+//numbar , string ("java script "),boolean (true ,false),undefindindesd (let age)
+//null(null)
+
+
+
+//dinaamic type langave type data
+// let score =321;
+// console.log("types of score is "+typeof(score))
+// let name ="ghgh";
+// console.log("types of score is "+typeof(name))
+// let num ;
+// console.log("types of score is "+typeof(num))
+// let jn =321;
+// console.log("types of score is "+typeof(jn))
+
+//uniory oprater
+// let a= 50;
+// let b = 20;
+
+
+
+
+// console.log("adddition of ",a);
+
+
+
+
+//ternary oprater
+let age=20;
+ let cont=(age>=18)?"voild age":"invaklid age";
+ console.log(cont)
